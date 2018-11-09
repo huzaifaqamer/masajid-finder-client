31e52d884ffc00ab4ff9c208a415aee79c137678
Huzaifa Qamer <huzaifaqamer@gmail.com>
2018-11-09 07:32:09 +0500

    initial commit

---
+++ src/pages/contribute/contribute.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-list',
+  templateUrl: 'contribute.html'
+})
+
+export class ContributePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
